53992f62aaf288f5a9520434bd97ef47d86834cf
gladok1123 <mecherakovvovam@gmail.com>
2026-01-31 12:29:12 +0700

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://ВАШ-ПРОЕКТ.supabase.co';
-const SUPABASE_ANON_KEY = 'ВАШ-АНОНИМНЫЙ-КЛЮЧ';
+const SUPABASE_URL = 'https://goziubuhrsamwzcvwogw.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdveml1YnVocnNhbXd6Y3Z3b2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MzEyMTgsImV4cCI6MjA4NTAwNzIxOH0.TVZaFlmWaepg8TrANM0E_LY6f9Ozqdg4SyNS7uGlQGs';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
