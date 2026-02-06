cb921a8648aee66062004c80a9ff19e3a4c3c7d9
gladok1123 <mecherakovvovam@gmail.com>
2026-02-06 18:46:26 +0700

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,3 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-const SUPABASE_URL = 'https://goziubuhrsamwzcvwogw.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdveml1YnVocnNhbXd6Y3Z3b2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MzEyMTgsImV4cCI6MjA4NTAwNzIxOH0.TVZaFlmWaepg8TrANM0E_LY6f9Ozqdg4SyNS7uGlQGs';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
+// supabase.js — не коммить в GitHub, если сделаешь публичным!
+const supabaseUrl = 'https://goziubuhrsamwzcvwogw.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdveml1YnVocnNhbXd6Y3Z3b2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MzEyMTgsImV4cCI6MjA4NTAwNzIxOH0.TVZaFlmWaepg8TrANM0E_LY6f9Ozqdg4SyNS7uGlQGs';
